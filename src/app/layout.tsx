9ecf6725a55ad4b29007fab02a0d5fa43316f5ca
Vinay Nain <vinaynain2601@gmail.com>
2026-02-15 10:56:29 +0530

    Addition of vercel analytics

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import SmoothScrolling from "@/lib/lenis";
+import { Analytics } from "@vercel/analytics/next"
 
 
 const geistSans = Geist({
@@ -29,6 +30,7 @@ export default function RootLayout({
       <body className={`${geistSans.variable} ${geistMono.variable}  font-sans`}>
         <SmoothScrolling/>
         {children}
+        <Analytics />
       </body>
     </html>
   );
